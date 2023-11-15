0ddbaa7b8d6e9bb2b081d305edcf608c4262fa91
Robert Jackson <rjackson@Roberts-MBP-3.routerlogin.net>
2023-11-15 13:08:27 -0600

    ADD: Fix with windows carriage returns

--- send_scan_to_lidar.py
+++ send_scan_to_lidar.py
@@ -1,36 +1,88 @@
 import paramiko
 import numpy as np
 
-def make_scan_file(azimuths, elevations, out_file_name):
+def make_scan_file(repeat, rays_per_point, 
+                   elevations, azimuths, out_file_name,
+                   beam_width=1):
     """
     Makes a scanning strategy file for a Halo Photonics Doppler Lidar.
     
     Parameters
     ----------
-    azimuths: float 1d array
-        The azimuth of each ray.
-    elevations: float 1d array
-        The elevation of each ray in the scan.
+    repeat: int
+        Number of times to repeat
+    no_points: int
+        The number of points to collect in the ray
+    rays_per_point: float
+        The number of rays per point
+    no_sweeps: int
+        The number of sweeps in the strategy
+    elevations: float 1d array or tuple
+        The elevation of each sweep in the scan. If this is a 2-tuple, then
+        the script will generate an RHI spanning the smallest to largest elevation
+    azimuths: float or 2-tuple
+        If this is a 2-tuple, then this script will generate a PPI from min_azi to max_azi.
     out_file_name: str
         The output name of the file.
-    """    
-    if not len(azimuths) == len(elevations):
-        raise ValueError("Azimuths and elevations must have same shape!")
+    beam_width: float
+        The spacing between beams.
     
-    with open(out_file_name, 'w') as fi:
-        for i in range(len(azimuths)):
-            string = "%07.3f%07.3f\n" % (azimuths[i], elevations[i])
-            print(string)
-            fi.write(string) 
+    """    
+    # PPI scan
+    if isinstance(azimuths, tuple):
+        min_azi = azimuths[0]
+        max_azi = azimuths[1]
 
-out_file_name = 'user4.txt'
+        if max_azi < min_azi:
+            no_points = int((max_azi + 360.0 - min_azi)/beam_width)
+        else:
+            no_points = int((max_azi - min_azi)/beam_width)
+        no_sweeps = len(elevations)
+        no_points = no_points * no_sweeps
+        with open(out_file_name, 'w') as fi:
+            fi.write('%d\r\n' % repeat)
+            fi.write('%d\r\n' % no_points)
+            fi.write('%d\r\n' % rays_per_point)
+            for i in range(no_sweeps):
+                cur_azi = min_azi
+                while(not np.abs(cur_azi - max_azi) < beam_width):
+                    cur_azi = cur_azi + beam_width
+                    if cur_azi > 360.0:
+                        cur_azi = cur_azi - 360.0
+                    string = "%07.3f%07.3f\r\n" % (cur_azi, elevations[i])
+                    fi.write(string) 
+
+    if isinstance(elevations, tuple):
+        min_el = elevations[0]
+        max_el = elevations[1]
+
+        if max_el < min_el:
+            no_points = int((max_el + 360.0 - min_el)/beam_width)
+        else:
+            no_points = int((max_el - min_el)/beam_width)
+        
+        with open(out_file_name, 'w') as fi:
+            fi.write('%d\r\n' % repeat)
+            fi.write('%d\r\n' % no_points)
+            fi.write('%d\r\n' % rays_per_point)
+            
+            cur_el = min_el
+            while(not np.abs(cur_el - max_el) < beam_width):
+                cur_el = cur_el + beam_width
+                if cur_el > 180.0:
+                    cur_el = cur_el - 180.0
+                string = "%07.3f%07.3f\r\n" % (azimuths, cur_el)
+                fi.write(string) 
+
+out_file_name = 'user.txt'
 lidar_ip_addr = '192.168.1.90'
 lidar_uname = 'waggle'
 lidar_pwd = 'w8ggl3'
 
-azimuths = np.arange(0, 60., 1.)
-elevations = 4 * np.ones_like(azimuths)
-make_scan_file(azimuths, elevations, out_file_name)
+rays_per_point = 1.
+azimuths = 60.
+elevations = (0., 90.)
+make_scan_file(7., rays_per_point, elevations, azimuths, out_file_name)
 
 with paramiko.SSHClient() as ssh:
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
